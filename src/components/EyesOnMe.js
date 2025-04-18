de7ce9decb4414d2cca7bf158e3b3f2f2e2c1d6f
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-18 15:48:02 +0300

    refactor EyesOnMe and Keypad components for clarity and consistency

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,38 @@
 // Code EyesOnMe Component Here
+// import React from 'react';
+
+// function EyesOnMe(){
+//     function handleFocus() {console.log('Good!')} 
+//     function handleBlur () {console.log('Hey! Eyes on me!')}
+//     return(
+//         <div>
+//         <button onFocus={handleFocus} onBlur={handleBlur}>Eyes On Me</button>
+//         </div>
+//     )
+// }
+// export default EyesOnMe;
+
+
+
+import React from 'react';
+
+function EyesOnMe() {
+  function handleFocus() {
+    console.log('Good!');
+  }
+
+  function handleBlur() {
+    console.log('Hey! Eyes on me!');
+  }
+
+  return (
+    <button 
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,11 +1,11 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
-    const handleChange = () => console.log("Entering password...")
+    function handleChange() {console.log("Entering password...")}
     return (
         <div>
-            <input type="password" onChange={handleChange}>
-            </input>
+            <input type="password" onChange={handleChange} />
         </div>
     )
 }
